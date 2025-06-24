2aeba49d6a47e6b873738fd67be5ea831514d248
LeeMinJu <mmnnii0276@gmail.com>
2025-06-24 16:02:57 +0900

    🔥 remove no use classes

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/attribute/service/AttributeService.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/attribute/service/AttributeService.java
@@ -1,10 +1,8 @@
 package com.part4.team05.sb01otbooteam05.domain.attribute.service;
 
-import com.part4.team05.sb01otbooteam05.domain.attribute.dto.AttributeDto;
 import com.part4.team05.sb01otbooteam05.domain.attribute.entity.Attribute;
 import com.part4.team05.sb01otbooteam05.domain.attribute.entity.AttributeDefinition;
 import com.part4.team05.sb01otbooteam05.domain.attribute.exception.NoSuchDefException;
-import com.part4.team05.sb01otbooteam05.domain.attribute.mapper.AttributeMapper;
 import com.part4.team05.sb01otbooteam05.domain.attribute.repository.AttributeDefinitionRepository;
 import com.part4.team05.sb01otbooteam05.domain.attribute.repository.AttributeRepository;
 import com.part4.team05.sb01otbooteam05.domain.clothes.entity.Clothes;
@@ -19,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class AttributeService {
   private final AttributeRepository attributeRepository;
   private final AttributeDefinitionRepository definitionRepository;
-  private final AttributeMapper attributeMapper;
 
   public List<Attribute> createAndReturnList(List<Attribute> attributes, Clothes clothes){
     return attributes.stream().map(attribute ->{
