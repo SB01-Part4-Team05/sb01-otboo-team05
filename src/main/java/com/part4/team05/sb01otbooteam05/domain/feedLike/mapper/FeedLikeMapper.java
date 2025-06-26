4254119723e408800c9a227cd044477afa668f77
kaya <answerme0622@daum.net>
2025-06-26 09:47:10 +0900

    :sparkles: 피드 관련 도메인 Mapper 클래스 구현

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feedLike/mapper/FeedLikeMapper.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feedLike/mapper/FeedLikeMapper.java
@@ -1,5 +1,13 @@
 package com.part4.team05.sb01otbooteam05.domain.feedLike.mapper;
 
-public class FeedLikeMapper {
+import org.mapstruct.Mapper;
 
+import com.part4.team05.sb01otbooteam05.domain.feedLike.dto.FeedLikeDto;
+import com.part4.team05.sb01otbooteam05.domain.feedLike.entity.FeedLike;
+
+@Mapper(componentModel = "spring")
+public interface FeedLikeMapper {
+	FeedLikeDto toDto(FeedLike feedLike);
+
+	FeedLike toEntity(FeedLikeDto feedLikeDto);
 }
--- src/main/java/com/part4/team05/sb01otbooteam05/domain/ootd/mapper/OotdMapper.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/ootd/mapper/OotdMapper.java
@@ -1,5 +1,21 @@
 package com.part4.team05.sb01otbooteam05.domain.ootd.mapper;
 
-public class OotdMapper {
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.part4.team05.sb01otbooteam05.domain.ootd.dto.OotdDto;
+import com.part4.team05.sb01otbooteam05.domain.ootd.entity.Ootd;
+
+@Mapper(componentModel = "spring")
+public interface OotdMapper {
+	OotdDto toDto(Ootd ootd);
+
+	List<OotdDto> toDtoList(List<Ootd> ootds);
+
+	Ootd toEntity(OotdDto dto);
+
+	List<Ootd> toEntityList(List<OotdDto> dtos);
+
 
 }
