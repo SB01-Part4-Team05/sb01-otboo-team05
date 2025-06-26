93e6e691973cb730c4bd6ec595699734971a2a31
LeeSG-0114 <leesin0114@naver.com>
2025-06-26 18:46:01 +0900

    :sparkles: 스프링 배치 테이블 생성 및 스프링 배치 의존성 추가

--- src/main/java/com/part4/team05/sb01otbooteam05/Sb01OtbooTeam05Application.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/Sb01OtbooTeam05Application.java
@@ -1,11 +1,13 @@
 package com.part4.team05.sb01otbooteam05;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+// @EnableBatchProcessing
 public class Sb01OtbooTeam05Application {
 
 	public static void main(String[] args) {
