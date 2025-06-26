7c39fda1550c7ed437cdbf1f883bd91a641226a4
kaya <answerme0622@daum.net>
2025-06-26 09:50:50 +0900

    :sparkles: 피드 댓글 클래스들 이름변경. FeedComment~ -> Comment~ 스웨거 명세와 일치시켜, 통일성을 주기 위함.

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feedComment/dto/FeedCommentsPageResponse.java
+++
@@ -1,4 +0,0 @@
-package com.part4.team05.sb01otbooteam05.domain.feedComment.dto;
-
-public record FeedCommentsPageResponse() {
-}
--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feedComment/dto/request/FindCommentsRequest.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feedComment/dto/request/FindCommentsRequest.java
@@ -1,4 +1,5 @@
 package com.part4.team05.sb01otbooteam05.domain.feedComment.dto.request;
 
-public record CreateFeedCommentRequest() {
+//todo 파라미터 묶어두기용
+public record FindCommentsRequest() {
 }
