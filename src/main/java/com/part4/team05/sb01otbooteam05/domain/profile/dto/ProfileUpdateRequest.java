e332ed1023e1775565365605a94ca4d2788a070c
jjhparkk <nomoredkdlel@gmail.com>
2025-06-26 16:20:02 +0900

    :fire: 파일 구조 변경

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/profile/dto/ProfileUpdateRequest.java
+++
@@ -1,5 +0,0 @@
-package com.part4.team05.sb01otbooteam05.domain.profile.dto;
-
-public class ProfileUpdateRequest {
-
-}
--- src/main/java/com/part4/team05/sb01otbooteam05/domain/profile/mapper/ProfileMapper.java
+++
@@ -1,4 +0,0 @@
-package com.part4.team05.sb01otbooteam05.domain.profile.mapper;
-
-public class ProfileMapper {
-}
