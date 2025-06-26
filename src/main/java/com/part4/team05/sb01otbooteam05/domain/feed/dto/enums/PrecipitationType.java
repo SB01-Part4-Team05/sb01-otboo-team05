44b21edd7f7667ff9c0bed79352ea450760ac75f
kaya <answerme0622@daum.net>
2025-06-26 11:51:14 +0900

    :sparkles: 피드 조회 관련 enum 타입 추가

---
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/dto/enums/PrecipitationType.java
@@ -0,0 +1,10 @@
+package com.part4.team05.sb01otbooteam05.domain.feed.dto.enums;
+
+public enum PrecipitationType {
+	//todo 날씨 도메인으로 옮길지 말지 고민해야함.
+	NONE,
+	RAIN,
+	RAIN_SNOW,
+	SNOW,
+	SHOWER
+}
---
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/dto/enums/SortType.java
@@ -0,0 +1,6 @@
+package com.part4.team05.sb01otbooteam05.domain.feed.dto.enums;
+
+public enum SortType {
+	createdAt,
+	likeCount
+}
