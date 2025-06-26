d84c1ede31946625c9ffb7035b5705ca7b7418f0
kaya <answerme0622@daum.net>
2025-06-26 15:23:46 +0900

    :sparkles: 전역 예외처리 클래스 추가

--- src/main/java/com/part4/team05/sb01otbooteam05/exception/ErrorCode.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/exception/ErrorCode.java
@@ -1,4 +1,22 @@
 package com.part4.team05.sb01otbooteam05.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ErrorCode {
+
+	// User 관련 에러 코드
+	USER_NOT_FOUND("해당 유저를 찾을 수 없습니다."),
+
+	// Feed 관련 에러 코드
+	FEED_NOT_FOUND("해당 피드를 찾을 수 없습니다."),
+
+
+	// 서버 관련 에러 코드
+	INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
+	INVALID_REQUEST("잘못된 요청입니다.");
+
+	private final String message;
 }
