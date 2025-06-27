bdb4d2a7dee74083164c4f3cc6cf575d559a5af0
kaya <answerme0622@daum.net>
2025-06-27 17:18:12 +0900

    :art: Dto 유효성검증 추가

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/dto/FeedsPageResponse.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/dto/FeedsPageResponse.java
@@ -4,13 +4,27 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 
+import jakarta.validation.constraints.NotNull;
+
 public record FeedsPageResponse(
+
+	@NotNull
 	List<FeedDto> data,
+
 	String nextCursor,
+
 	String IdAfter,
+
+	@NotNull
 	Boolean hasNext,
+
+	@NotNull
 	Long totalCount,
+
+	@NotNull
 	String sortBy,
+
+	@NotNull
 	Sort.Direction direction
 ) {
 }
--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feedComment/dto/CommentsPageResponse.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feedComment/dto/CommentsPageResponse.java
@@ -4,13 +4,27 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CommentsPageResponse(
+
+	@NotNull
 	List<CommentDto> comments,
+
 	String nextCursor,
+
 	String nextIdAfter,
+
+	@NotNull
 	Boolean hasNext,
+
+	@NotNull
 	Long totalCount,
+
+	@NotNull
 	String sortBy,
+
+	@NotNull
 	Sort.Direction direction
 ) {
 }
