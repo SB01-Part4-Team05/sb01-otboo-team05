5e9a2692feb09b12c1c1990de0f348d5da55eafa
kaya <answerme0622@daum.net>
2025-06-27 14:28:23 +0900

    :art: enums 패키지 위치 변경

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/dto/enums/SortType.java
+++
@@ -1,6 +0,0 @@
-package com.part4.team05.sb01otbooteam05.domain.feed.dto.enums;
-
-public enum SortType {
-	createdAt,
-	likeCount
-}
--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/enums/SkyStatusType.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/enums/SkyStatusType.java
@@ -1,4 +1,4 @@
-package com.part4.team05.sb01otbooteam05.domain.feed.dto.enums;
+package com.part4.team05.sb01otbooteam05.domain.feed.enums;
 
 public enum SkyStatusType {
 	//todo 날씨 도메인으로 옮길지 말지 고민해야함.
