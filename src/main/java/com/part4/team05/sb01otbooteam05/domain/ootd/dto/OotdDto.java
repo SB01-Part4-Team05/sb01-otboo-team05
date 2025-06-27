a3bb3d4ca185e163157d5cdffe59e2e4db07b9ab
kaya <answerme0622@daum.net>
2025-06-27 14:42:24 +0900

    :sparkles: AuthorDto 생성 및 피드 관련 Dto 필드값 수정

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/ootd/dto/OotdDto.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/ootd/dto/OotdDto.java
@@ -3,13 +3,13 @@ package com.part4.team05.sb01otbooteam05.domain.ootd.dto;
 import java.util.List;
 import java.util.UUID;
 
-import com.part4.team05.sb01otbooteam05.domain.attribute.entity.Attribute;
+import com.part4.team05.sb01otbooteam05.domain.attribute.dto.AttributeDto;
 
 public record OotdDto(
 	UUID clothesID,
 	String name,
 	String imageUrl,
 	String type,
-	List<Attribute> attributes
+	List<AttributeDto> attributes
 ) {
 }
