38b53766c1acc3363eb579f560e138990c111cd4
LeeSG-0114 <leesin0114@naver.com>
2025-06-30 23:17:12 +0900

    :sparkles: 기상청api 응답 dto 추가

---
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/dto/WeatherResponse.java
@@ -0,0 +1,53 @@
+package com.part4.team05.sb01otbooteam05.domain.weather.dto;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WeatherResponse{
+
+  private Response response;
+
+  @Getter
+  @Setter
+  public static class Response {
+    private Header header;
+    private Body body;
+  }
+
+  @Getter
+  @Setter
+  public static class Header {
+    private String resultCode;
+    private String resultMsg;
+  }
+
+  @Getter
+  @Setter
+  public static class Body {
+    private String dataType;
+    private Items items;
+  }
+
+  @Getter
+  @Setter
+  public static class Items {
+    private List<Item> item;
+  }
+
+  @Getter
+  @Setter
+  public static class Item {
+    private String baseDate;
+    private String baseTime;
+    private String category;
+    private String fcstDate;
+    private String fcstTime;
+    private String fcstValue;
+    private int nx;
+    private int ny;
+  }
+
+}
