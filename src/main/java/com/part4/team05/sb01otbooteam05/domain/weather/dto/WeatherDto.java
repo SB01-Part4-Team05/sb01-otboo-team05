e3c198fe5515722d45b7648676b3897d8c4e3976
LeeSG-0114 <leesin0114@naver.com>
2025-07-01 13:16:53 +0900

    :sparkles: 날씨 관련 dto 내부 필드 및 구조 정의

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/dto/WeatherDto.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/dto/WeatherDto.java
@@ -1,5 +1,32 @@
 package com.part4.team05.sb01otbooteam05.domain.weather.dto;
 
-public record WeatherDto {
+import com.part4.team05.sb01otbooteam05.domain.weather.entity.SkyStatusType;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record WeatherDto(
+    @NotNull
+    UUID id,
+
+    @NotNull
+    LocalDateTime forecastedAt,
+
+    @NotNull
+    LocalDateTime forecastAt,
+
+    WeatherAPILocation location,
+
+    @NotNull
+    SkyStatusType skyStatus,
+
+    PrecipitationDto precipitation,
+
+    HumidityDto humidity,
+
+    TemperatureDto temperature,
+
+    WindSpeedDto windSpeed
+) {
 
 }
