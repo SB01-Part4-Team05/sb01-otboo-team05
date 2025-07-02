ff2925472206240ca57664870e3460e3b39db460
LeeMinJu <mmnnii0276@gmail.com>
2025-07-02 14:18:00 +0900

    :sparkles: make recommend controller & ci.yml

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/recommend/service/RecommendService.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/recommend/service/RecommendService.java
@@ -2,8 +2,10 @@ package com.part4.team05.sb01otbooteam05.domain.recommend.service;
 
 import com.part4.team05.sb01otbooteam05.domain.attribute.entity.StyleType;
 import com.part4.team05.sb01otbooteam05.domain.attribute.entity.ThicknessType;
+import com.part4.team05.sb01otbooteam05.domain.clothes.dto.ClothesDto;
 import com.part4.team05.sb01otbooteam05.domain.clothes.entity.Clothes;
 import com.part4.team05.sb01otbooteam05.domain.clothes.entity.ClothesType;
+import com.part4.team05.sb01otbooteam05.domain.clothes.mapper.ClothesMapper;
 import com.part4.team05.sb01otbooteam05.domain.clothes.service.ClothesService;
 import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -23,11 +25,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RecommendService {
   private final ClothesService clothesService;
-
+  private final ClothesMapper clothesMapper;
   private final Random random = new Random();
   private final Map<ThicknessType,Integer> criteria = new HashMap<>(); // 옷 두께 가중치
 
-  public List<List<Clothes>> getRecommend(UUID ownerId){
+  public List<List<ClothesDto>> getRecommend(UUID ownerId, UUID weatherId){
     Map<StyleType, Map<ClothesType,List<Clothes>>> map = getMap(ownerId);
     List<List<Clothes>> result = new ArrayList<>();
 
@@ -67,9 +69,16 @@ public class RecommendService {
       result.addAll(settings);
     }
 
-    return result;
+    List<List<ClothesDto>> finalResult = new ArrayList<>();
+
+    for(List<Clothes> list : result){
+      finalResult.add(clothesMapper.toDtoList(list));
+    }
+
+    return finalResult;
   }
 
+  // 원피스 가중치 점수
   private List<List<Clothes>> getDressScore(List<Clothes> dresses,List<Clothes> outers) {
     List<List<Clothes>> list = new ArrayList<>();
 
@@ -89,6 +98,7 @@ public class RecommendService {
     return list;
   }
 
+  // 유저가 가진 옷을 스타일별로 분류한 뒤 다시 의상 타입별로 분류한 맵 생성
   private Map<StyleType, Map<ClothesType,List<Clothes>>> getMap(UUID ownerId){
     List<Clothes> allClothes = clothesService.findAllByOwnerId(ownerId);
 
@@ -104,6 +114,7 @@ public class RecommendService {
         ));
   }
 
+  // 가중치 계산
   private int getWeight(Clothes clothes){
     return clothes.getAttributeValues().stream()
         .filter(attributeValue -> "thickness".equals(attributeValue.getDefinition().getName()))
@@ -120,6 +131,7 @@ public class RecommendService {
         .orElse(0);
   }
 
+  // 옷에 부여하는 기준 가중치
   @PostConstruct
   public void makeCriteria(){
     criteria.put(ThicknessType.THICK, 5);
@@ -128,6 +140,7 @@ public class RecommendService {
     criteria.put(ThicknessType.THIN,-5);
   }
 
+  // 상+하의 조합별 가중치 점수
   private Map<Clothes,List<Integer>> getScore(List<Clothes> tops, List<Clothes> bottoms){
     Map<Clothes,List<Integer>> map = new HashMap<>();
 
@@ -144,6 +157,7 @@ public class RecommendService {
     return map;
   }
 
+  // 추천 세트 리스트
   private List<List<Clothes>>  getSetting(Map<Clothes,List<Integer>> map, List<Clothes> bottoms
   ,List<Clothes> outers){
     List<List<Clothes>> result = new ArrayList<>();
@@ -166,6 +180,7 @@ public class RecommendService {
     return result;
   }
 
+  // 상+하의 나 원피스와 아우터를 같이 추천하기 위함
   private List<Clothes> withOuter(int totalScore, List<Clothes> outers){
     List<Clothes> withOuter = new ArrayList<>();
 
@@ -182,19 +197,11 @@ public class RecommendService {
 
   /*
   추천 기준
-    최저 - 최고 중간 값을 기준
-    속성 값을 통해 추천 -> 두께에 가중치를 둬서 특정 가중치가 나오도록 조합
-    의상을 두께 인덱스로 정렬할 필요성이 있는가?
-    유저 ID를 통해서 옷 리스트를 가지고 옴 -> 타입별로 나눔 -> 가중치 별로 뽑음.. 효율적인지?
-    => 상+하 의 가중치만 보기 ++아우터는 선택사항.. + 악세사리 없을 때 / 있을 때
-    => 악세사리는 같은 스타일 타입에 존재하는 악세사리 3개 랜덤 선택 후 추천 세트에 포함시키기
      날씨에서 가져올 것
      온도 최고 / 최저
      온도 미디언 값 -> 가중치 확인
      온도 가중치는 5도 단위로 끊고 -15 ~ 35 까지
      -15 / -10 / -7 / -5 / 2 / 5 / 7 / 10 / 12 / 15
      옷 가중치 + 온도 가중치가 0 ~ 5 사이가 되도록 하는 것이 추천의 목표
-
-     score = -Math.abs(상의 - 하의) - 상하의 가중치 차이값 <= 허용 오차
    */
 }
