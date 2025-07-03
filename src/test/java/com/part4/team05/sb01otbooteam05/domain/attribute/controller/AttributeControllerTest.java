26819cb452eef0f15fe69a9c8d406b8bc7e92e68
LeeMinJu <mmnnii0276@gmail.com>
2025-07-03 14:10:05 +0900

    :fire: chore attribute controller test code

--- src/test/java/com/part4/team05/sb01otbooteam05/domain/attribute/controller/AttributeControllerTest.java
+++ src/test/java/com/part4/team05/sb01otbooteam05/domain/attribute/controller/AttributeControllerTest.java
@@ -1,39 +1,39 @@
-package com.part4.team05.sb01otbooteam05.domain.attribute.controller;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.part4.team05.sb01otbooteam05.domain.attribute.service.AttributeService;
-import com.part4.team05.sb01otbooteam05.domain.clothes.dto.ClothesAttributeDefCreateRequest;
-import com.part4.team05.sb01otbooteam05.domain.user.entity.User;
-import java.util.List;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.assertj.MockMvcTester.MockMvcRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-@WebMvcTest(controllers = AttributeController.class)
-@ExtendWith(MockitoExtension.class)
-class AttributeControllerTest {
-
-  @Autowired
-  MockMvc mockMvc;
-
-  @Autowired
-  ObjectMapper objectMapper;
-
-  @MockitoBean
-  AttributeService attributeService;
-
+//package com.part4.team05.sb01otbooteam05.domain.attribute.controller;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.mockito.Mockito.mock;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.part4.team05.sb01otbooteam05.domain.attribute.service.AttributeService;
+//import com.part4.team05.sb01otbooteam05.domain.clothes.dto.ClothesAttributeDefCreateRequest;
+//import com.part4.team05.sb01otbooteam05.domain.user.entity.User;
+//import java.util.List;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.test.context.support.WithMockUser;
+//import org.springframework.test.context.bean.override.mockito.MockitoBean;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.assertj.MockMvcTester.MockMvcRequestBuilder;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//@WebMvcTest(controllers = AttributeController.class)
+//@ExtendWith(MockitoExtension.class)
+//class AttributeControllerTest {
+//
+//  @Autowired
+//  MockMvc mockMvc;
+//
+//  @Autowired
+//  ObjectMapper objectMapper;
+//
+//  @MockitoBean
+//  AttributeService attributeService;
+//
 //  @Test
 //  @WithMockUser(roles = "ADMIN")
 //  void createDef() throws Exception{
@@ -45,16 +45,16 @@ class AttributeControllerTest {
 //            .contentType(MediaType.APPLICATION_JSON))
 //        .andExpect(status().isCreated());
 //  }
-
-  @Test
-  void update() {
-  }
-
-  @Test
-  void deleteDef() {
-  }
-
-  @Test
-  void getDef() {
-  }
-}
+//
+//  @Test
+//  void update() {
+//  }
+//
+//  @Test
+//  void deleteDef() {
+//  }
+//
+//  @Test
+//  void getDef() {
+//  }
+//}
