ad838adcaa3a783cbb888ffc59af3c75e47a9c69
kaya <answerme0622@daum.net>
2025-07-06 01:49:17 +0900

    :art: Feed 엔티티->Dto 매퍼 메서드 변환로직 변경

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/mapper/FeedMapper.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/feed/mapper/FeedMapper.java
@@ -4,17 +4,42 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 
+import com.part4.team05.sb01otbooteam05.domain.feed.dto.AuthorDto;
 import com.part4.team05.sb01otbooteam05.domain.feed.dto.FeedDto;
 import com.part4.team05.sb01otbooteam05.domain.feed.entity.Feed;
+import com.part4.team05.sb01otbooteam05.domain.ootd.dto.OotdDto;
+import com.part4.team05.sb01otbooteam05.domain.ootd.entity.Ootd;
+import com.part4.team05.sb01otbooteam05.domain.ootd.mapper.OotdMapper;
+import com.part4.team05.sb01otbooteam05.domain.user.entity.User;
+import com.part4.team05.sb01otbooteam05.domain.weather.Mapper.WeatherMapper;
+import com.part4.team05.sb01otbooteam05.domain.weather.dto.WeatherDto;
+import com.part4.team05.sb01otbooteam05.domain.weather.entity.Weather;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {WeatherMapper.class, OotdMapper.class})
 public interface FeedMapper {
-	FeedDto toDto(Feed feed);
 
 	List<FeedDto> toDtoList(List<Feed> feeds);
 
-	Feed toEntity(FeedDto feedDto);
+	AuthorDto toDto(User user);
 
-	List<Feed> toEntityList(List<FeedDto> feedDtos);
+	WeatherDto toDto(Weather weather);
 
+	OotdDto toDto(Ootd ootd);
+
+	List<OotdDto> toOotdDtoList(List<Ootd> ootds);
+
+	default FeedDto toDto(Feed feed, Long likeCount, Integer commentCount, Boolean likedByMe) {
+		return new FeedDto(
+			feed.getId(),
+			feed.getCreatedAt(),
+			feed.getUpdatedAt(),
+			toDto(feed.getAuthor()),
+			toDto(feed.getWeather()),
+			toOotdDtoList(feed.getOotds()),
+			feed.getContent(),
+			likeCount,
+			commentCount,
+			likedByMe
+		);
+	}
 }
