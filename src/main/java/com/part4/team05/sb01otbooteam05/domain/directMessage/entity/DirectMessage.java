12405a8b4f4df5afcf83d461ca3ee6eb9fbba68f
mingi96 <minnet1030@naver.com>
2025-07-07 18:43:36 +0900

    :adhesive_bandage: fix: Lombok 어노테이션 추가 및 타임스탬프 타입 통일

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/directMessage/entity/DirectMessage.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/directMessage/entity/DirectMessage.java
@@ -3,7 +3,14 @@ package com.part4.team05.sb01otbooteam05.domain.directMessage.entity;
 import com.part4.team05.sb01otbooteam05.domain.base.BaseEntity;
 import com.part4.team05.sb01otbooteam05.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "direct_messages")
 public class DirectMessage extends BaseEntity {
