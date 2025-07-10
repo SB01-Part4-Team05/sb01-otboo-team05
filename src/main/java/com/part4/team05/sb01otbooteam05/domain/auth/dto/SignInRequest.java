d695c06acba1a1976e1b0b63f32cf92f5e5069be
jjhparkk <nomoredkdlel@gmail.com>
2025-07-10 17:52:44 +0900

    :sparkles: Auth 관련 DTO 추가

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/auth/dto/SignInRequest.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/auth/dto/SignInRequest.java
@@ -1,5 +1,13 @@
 package com.part4.team05.sb01otbooteam05.domain.auth.dto;
 
-public class SignInRequest {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
-}
+public record SignInRequest(
+    @NotBlank(message = "이메일은 필수입니다")
+    @Email(message = "올바른 이메일 형식이 아닙니다")
+    String email,
+
+    @NotBlank(message = "비밀번호는 필수입니다")
+    String password
+) {}
