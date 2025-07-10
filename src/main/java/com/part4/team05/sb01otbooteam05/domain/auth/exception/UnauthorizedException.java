ed98a18aead43f76eb867f52fb9422b500e14751
jjhparkk <nomoredkdlel@gmail.com>
2025-07-10 17:55:55 +0900

    :sparkles: Auth 관련 기본 예외 추가

---
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/auth/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package com.part4.team05.sb01otbooteam05.domain.auth.exception;
+
+import com.part4.team05.sb01otbooteam05.exception.ErrorCode;
+import com.part4.team05.sb01otbooteam05.exception.OtbooException;
+
+public class UnauthorizedException extends OtbooException {
+
+  public UnauthorizedException(String message) {
+    super(ErrorCode.UNAUTHORIZED);
+  }
+}
