7055b612d1e82b27205710d2d5b61be278826698
mingi96 <minnet1030@naver.com>
2025-07-10 18:37:09 +0900

    :recycle: refactor: JpaConfig 클래스 제거

--- src/main/java/com/part4/team05/sb01otbooteam05/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.part4.team05.sb01otbooteam05.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
