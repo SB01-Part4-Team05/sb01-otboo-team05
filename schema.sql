9a7108da6686c7398a4f9f45d76d42c8362b2dcb
LeeSG-0114 <leesin0114@naver.com>
2025-07-11 11:14:43 +0900

    :sparkles: 날씨 테이블 인덱스 추가

--- schema.sql
+++ schema.sql
@@ -170,6 +170,7 @@ CREATE TABLE feed_likes
 CREATE INDEX idx_users_email ON users(email);
 CREATE INDEX idx_refresh_tokens_token ON refresh_tokens(token);
 CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens(user_id);
+CREATE INDEX idx_weather_location_forecast ON weathers(location_x, location_y, forecast_at);
 
 -- 스프링 배치 테이블
 CREATE TABLE BATCH_JOB_INSTANCE  (
