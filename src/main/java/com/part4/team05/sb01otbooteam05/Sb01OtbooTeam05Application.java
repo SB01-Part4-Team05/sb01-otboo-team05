3f4fa5c1aeb42f0cec60d68db2d4cf8e0a17ba3b
LeeSG-0114 <leesin0114@naver.com>
2025-07-11 18:05:06 +0900

    :sparkles: 스케줄링 어노테이션 추가

--- src/main/java/com/part4/team05/sb01otbooteam05/Sb01OtbooTeam05Application.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/Sb01OtbooTeam05Application.java
@@ -1,15 +1,16 @@
 package com.part4.team05.sb01otbooteam05;
 
 import com.part4.team05.sb01otbooteam05.domain.auth.config.JwtProperties;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableJpaAuditing
+@EnableScheduling
 @EnableConfigurationProperties(JwtProperties.class)
 public class Sb01OtbooTeam05Application {
 
