a604d5797af0f4d0ea25150ca390fd0b65e2cb3e
LeeMinJu <mmnnii0276@gmail.com>
2025-07-15 13:17:13 +0900

    :recycle: change random userId to login userId

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/recommend/controller/RecommendController.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/recommend/controller/RecommendController.java
@@ -1,12 +1,16 @@
 package com.part4.team05.sb01otbooteam05.domain.recommend.controller;
 
+import com.part4.team05.sb01otbooteam05.domain.auth.security.CustomUserDetails;
 import com.part4.team05.sb01otbooteam05.domain.clothes.dto.ClothesDto;
 import com.part4.team05.sb01otbooteam05.domain.recommend.service.RecommendService;
 import java.util.List;
 import java.util.UUID;
+import javax.security.sasl.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,8 +24,18 @@ public class RecommendController {
   private final RecommendService recommendService;
 
   @GetMapping
-  public ResponseEntity<List<List<ClothesDto>>> getRecommendSet(@RequestParam UUID weatherId){
-    UUID userId = UUID.randomUUID(); // 교체 필요
+  public ResponseEntity<List<List<ClothesDto>>> getRecommendSet(@RequestParam UUID weatherId)
+      throws AuthenticationException {
+    UUID userId;
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails) {
+      CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+      userId = userDetails.getUserId();
+    } else {
+      throw new AuthenticationException("로그인 된 유저가 없습니다.");
+    }
+
     return ResponseEntity.ok(recommendService.getRecommend(userId,weatherId));
   }
 
