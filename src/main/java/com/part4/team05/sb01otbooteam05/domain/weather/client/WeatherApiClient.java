a512ca99f626513985bcd20676ce73879cebfba4
LeeSG-0114 <leesin0114@naver.com>
2025-07-15 16:24:48 +0900

    :package: client 패키지 생성 및 이동

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/client/WeatherApiClient.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/client/WeatherApiClient.java
@@ -1,4 +1,4 @@
-package com.part4.team05.sb01otbooteam05.domain.weather;
+package com.part4.team05.sb01otbooteam05.domain.weather.client;
 
 import com.part4.team05.sb01otbooteam05.domain.weather.dto.ParsedForecastDto;
 import com.part4.team05.sb01otbooteam05.domain.weather.dto.WeatherResponse;
