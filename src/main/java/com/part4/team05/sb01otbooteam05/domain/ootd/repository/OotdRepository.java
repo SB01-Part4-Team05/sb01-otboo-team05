75e382a3e19b3ebd355f38fb198b32f2f29467df
김희수_kaya <92302468+kaya-frog-ramer@users.noreply.github.com>
2025-07-22 17:01:55 +0900

    :poop: 테스트 저장용 임시커밋

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/ootd/repository/OotdRepository.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/ootd/repository/OotdRepository.java
@@ -1,5 +1,11 @@
 package com.part4.team05.sb01otbooteam05.domain.ootd.repository;
 
-public class OotdRepository {
+import com.part4.team05.sb01otbooteam05.domain.ootd.entity.Ootd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OotdRepository extends JpaRepository<Ootd, UUID> {
+
 
 }
--- src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/mapper/WeatherMapper.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/mapper/WeatherMapper.java
@@ -1,12 +1,6 @@
 package com.part4.team05.sb01otbooteam05.domain.weather.mapper;
 
-import com.part4.team05.sb01otbooteam05.domain.weather.dto.HumidityDto;
-import com.part4.team05.sb01otbooteam05.domain.weather.dto.PrecipitationDto;
-import com.part4.team05.sb01otbooteam05.domain.weather.dto.TemperatureDto;
-import com.part4.team05.sb01otbooteam05.domain.weather.dto.WeatherAPILocation;
-import com.part4.team05.sb01otbooteam05.domain.weather.dto.WeatherDto;
-import com.part4.team05.sb01otbooteam05.domain.weather.dto.WeatherSummaryDto;
-import com.part4.team05.sb01otbooteam05.domain.weather.dto.WindSpeedDto;
+import com.part4.team05.sb01otbooteam05.domain.weather.dto.*;
 import com.part4.team05.sb01otbooteam05.domain.weather.entity.Weather;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -14,57 +8,58 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class WeatherMapper {
 
-	public static WeatherDto toDto(Weather weather, WeatherAPILocation weatherAPILocation) {
-		return new WeatherDto(
-			weather.getId(),
-			weather.getForecastedAt(),
-			weather.getForecastAt(),
-			weatherAPILocation,
-			weather.getSkyStatusType(),
-			toPrecipitationDto(weather),
-			toHumidityDto(weather),
-			toTemperatureDto(weather),
-			toWindSpeedDto(weather)
-		);
-	}
 
-	public static WeatherSummaryDto toSummaryDto(Weather weather) {
-		return new WeatherSummaryDto(
-			weather.getId(),
-			weather.getSkyStatusType(),
-			toPrecipitationDto(weather),
-			toTemperatureDto(weather)
-		);
-	}
+    public static WeatherDto toDto(Weather weather, WeatherAPILocation weatherAPILocation) {
+        return new WeatherDto(
+                weather.getId(),
+                weather.getForecastedAt(),
+                weather.getForecastAt(),
+                weatherAPILocation,
+                weather.getSkyStatusType(),
+                toPrecipitationDto(weather),
+                toHumidityDto(weather),
+                toTemperatureDto(weather),
+                toWindSpeedDto(weather)
+        );
+    }
 
-	public static WindSpeedDto toWindSpeedDto(Weather weather) {
-		return new WindSpeedDto(
-			weather.getWindSpeed(),
-			weather.getWindSpeedAsWord()
-		);
-	}
+    public static WeatherSummaryDto toSummaryDto(Weather weather) {
+        return new WeatherSummaryDto(
+                weather.getId(),
+                weather.getSkyStatusType(),
+                toPrecipitationDto(weather),
+                toTemperatureDto(weather)
+        );
+    }
 
-	public static TemperatureDto toTemperatureDto(Weather weather) {
-		return new TemperatureDto(
-			weather.getTemperatureCurrent(),
-			weather.getTemperatureComparedToDayBefore(),
-			weather.getTemperatureMin(),
-			weather.getTemperatureMax()
-		);
-	}
+    public static WindSpeedDto toWindSpeedDto(Weather weather) {
+        return new WindSpeedDto(
+                weather.getWindSpeed(),
+                weather.getWindSpeedAsWord()
+        );
+    }
 
-	public static PrecipitationDto toPrecipitationDto(Weather weather) {
-		return new PrecipitationDto(
-			weather.getPrecipitationType(),
-			weather.getPrecipitationAmount(),
-			weather.getPrecipitationProbability()
-		);
-	}
+    public static TemperatureDto toTemperatureDto(Weather weather) {
+        return new TemperatureDto(
+                weather.getTemperatureCurrent(),
+                weather.getTemperatureComparedToDayBefore(),
+                weather.getTemperatureMin(),
+                weather.getTemperatureMax()
+        );
+    }
 
-	public static HumidityDto toHumidityDto(Weather weather) {
-		return new HumidityDto(
-			weather.getHumidityCurrent(),
-			weather.getHumidityComparedToDayBefore()
-		);
-	}
+    public static PrecipitationDto toPrecipitationDto(Weather weather) {
+        return new PrecipitationDto(
+                weather.getPrecipitationType(),
+                weather.getPrecipitationAmount(),
+                weather.getPrecipitationProbability()
+        );
+    }
+
+    public static HumidityDto toHumidityDto(Weather weather) {
+        return new HumidityDto(
+                weather.getHumidityCurrent(),
+                weather.getHumidityComparedToDayBefore()
+        );
+    }
 }
