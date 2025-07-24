f4e0046fc9514bda573737a7ba13d184fc8c1a03
LeeSG-0114 <leesin0114@naver.com>
2025-07-25 08:59:39 +0900

    :recycle: 로컬타임 파싱 예외처리 추가

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/batch/reader/OldWeatherItemReader.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/weather/batch/reader/OldWeatherItemReader.java
@@ -1,6 +1,7 @@
 package com.part4.team05.sb01otbooteam05.domain.weather.batch.reader;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -22,13 +23,20 @@ public class OldWeatherItemReader extends JdbcPagingItemReader<UUID> {
       throw new IllegalArgumentException("deleteTime 파라미터는 필수입니다.");
     }
 
+    LocalDateTime deleteTime;
+    try {
+      deleteTime = LocalDateTime.parse(deleteTimeStr);
+    } catch (DateTimeParseException e) {
+      throw new IllegalArgumentException("deleteTime 파라미터 형식이 올바르지 않습니다.");
+    }
+
     this.setDataSource(dataSource);
     this.setPageSize(1000);
     this.setRowMapper((rs, rowNum) -> UUID.fromString(rs.getString("id")));
     this.setQueryProvider(createQueryProvider());
 
     Map<String, Object> parameters = new HashMap<>();
-    parameters.put("deleteTime", LocalDateTime.parse(deleteTimeStr));
+    parameters.put("deleteTime", deleteTime);
     this.setParameterValues(parameters);
   }
 
