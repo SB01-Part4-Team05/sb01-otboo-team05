7d3e929f4be716bb2a210ffab0cc19798f9964f1
LeeMinJu <mmnnii0276@gmail.com>
2025-07-29 11:39:19 +0900

    :bug: fix: error

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/attribute/service/AttributeService.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/attribute/service/AttributeService.java
@@ -15,6 +15,7 @@ import java.awt.print.Pageable;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -41,7 +42,7 @@ public class AttributeService {
       attributeRepository.save(att);
 
       return att;
-    }).toList();
+    }).collect(Collectors.toList());
   }
 
   @Transactional
