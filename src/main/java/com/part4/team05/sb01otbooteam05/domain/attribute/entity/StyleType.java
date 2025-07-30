feb907b9f49dc1dccb1986902bdf5a7a09ed99ee
LeeMinJu <mmnnii0276@gmail.com>
2025-07-30 15:48:03 +0900

    :bug: fix: error

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/attribute/entity/StyleType.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/attribute/entity/StyleType.java
@@ -1,20 +1,10 @@
 package com.part4.team05.sb01otbooteam05.domain.attribute.entity;
 
 public enum StyleType {
-  CASUAL("캐주얼"),
-  STREET("스트릿"),
-  SPORTY("스포티"),
-  MINIMAL("미니멀"),
-  CHIC("시크"),
-  RETRO("레트로");
-
-  private final String label;
-
-   StyleType(String label){
-     this.label = label;
-   }
-
-   public String getLabel(){
-     return label;
-   }
+  CASUAL,
+  STREET,
+  SPORTY,
+  MINIMAL,
+  CHIC,
+  RETRO
 }
