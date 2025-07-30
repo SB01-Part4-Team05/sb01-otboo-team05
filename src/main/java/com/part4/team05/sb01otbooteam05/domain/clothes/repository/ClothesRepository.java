8a9227847e94e5f0135f06ba76b281e8f9956445
LeeMinJu <mmnnii0276@gmail.com>
2025-07-30 15:21:38 +0900

    :bug: fix: error

--- src/main/java/com/part4/team05/sb01otbooteam05/domain/clothes/repository/ClothesRepository.java
+++ src/main/java/com/part4/team05/sb01otbooteam05/domain/clothes/repository/ClothesRepository.java
@@ -5,6 +5,7 @@ import com.part4.team05.sb01otbooteam05.domain.clothes.entity.ClothesType;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClothesRepository extends JpaRepository<Clothes, UUID> {
 
+  @EntityGraph(attributePaths = {"attributeValues"})
   List<Clothes> findByOwnerId(UUID ownerId);
 
   @Query("""
